4ec1f70019dc68daad2e18aaf4bc6d504c4b372d
avishekataur <avishekataur@gmail.com>
2021-08-15 13:14:47 +0600

    first commit

---
+++ js/bank.js
@@ -0,0 +1,22 @@
+document.getElementById('login-button').addEventListener('click',
+
+function()
+{
+    //For email
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+    console.log(userEmail);
+
+    //For password
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+    console.log(userPassword);
+
+    if(userEmail == 'abc@mail.com' && userPassword == 'secret')
+    {
+        window.location.href = 'bank.html';
+    }
+    
+}
+
+)
